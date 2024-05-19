9febf6e092beacfb90d9f2176e1aedfe48e7fb62
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:01:42 +0200

    Update Materiel.php

--- CasierConnecte/app/Models/Materiel.php
+++ CasierConnecte/app/Models/Materiel.php
@@ -6,23 +6,38 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Materiel extends Model
 {
     use HasFactory;
-    public $timestamps = false; // Désactiver les timestamps
+
+    // Désactiver les timestamps automatiques (created_at, updated_at)
+    public $timestamps = false;
+
+    // Définir les attributs pouvant être remplis en masse
     protected $fillable = ['nom', 'image', 'etat', 'duree_emprunt', 'id_famille_materiel', 'id_casier'];
+
+    // Définir la relation "un à plusieurs" avec le modèle Emprunt
+
     public function emprunt(): HasMany
-{
-    return $this->hasMany(Emprunt::class);
-}
-public function famille_materiel(): BelongsTo
-{
-    return $this->belongsTo(Famille_materiel::class);
-}
-public function casier(): BelongsTo
-{
-    return $this->belongsTo(Casier::class, 'id_meuble', 'id');
-}
+    {
+        return $this->hasMany(Emprunt::class);
+    }
+
+    
+    // Définir la relation "plusieurs à un" avec le modèle Famille_materiel
+     
+    public function famille_materiel(): BelongsTo
+    {
+        return $this->belongsTo(Famille_materiel::class);
+    }
+
+    /**
+     * Définir la relation "plusieurs à un" avec le modèle Casier
+     * Lier id_casier dans Materiel avec id dans Casier
+     */
+    public function casier(): BelongsTo
+    {
+        return $this->belongsTo(Casier::class, 'id_casier', 'id');
+    }
 }
