2ca400eb51d869ca40c8c2869c30b62861adbf6c
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:59:13 +0200

    Update EmpruntService.php

--- CasierConnecte/app/Services/EmpruntService.php
+++ CasierConnecte/app/Services/EmpruntService.php
@@ -17,21 +17,30 @@ class EmpruntService
         $this->smsService = $smsService;
     }
 
+    /**
+     * Vérifie les emprunts en retard et envoie des SMS aux adhérents concernés.
+     */
     public function verifierEmpruntsEnRetard()
     {
+        // Récupère tous les emprunts
         $emprunts = Emprunt::all();
 
         foreach ($emprunts as $emprunt) {
+            // Récupère la date de retour prévue de l'emprunt
             $dateRetourPrevue = Carbon::parse($emprunt->date_limite);
 
-            if ($dateRetourPrevue === false) {
-                continue;
-            }
-
+            // Vérifie si la date de retour prévue est passée et si l'emprunt n'a pas encore été retourné
             if ($dateRetourPrevue->isPast() && $emprunt->date_retour === null) {
+                // Récupère le numéro de téléphone de l'adhérent
                 $phoneNumber = $emprunt->adherent->num_telephone;
+
+                // Récupère le nom du matériel emprunté
                 $materielNom = $emprunt->materiel->nom;
+
+                // Crée le message SMS
                 $message = 'Bonjour, vous avez oublié de rendre votre matériel : "' . $materielNom . '". Merci de le retourner dès que possible. Ceci est un message automatique. Merci de ne pas y répondre.';
+
+                // Envoie le SMS
                 $this->smsService->sendSms($phoneNumber, $message);
             }
         }
