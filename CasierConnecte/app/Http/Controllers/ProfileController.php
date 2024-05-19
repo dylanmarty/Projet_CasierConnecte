0b8667da0fbe491ee5bedcd8ecffec5dfea403ac
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:49:06 +0200

    Update ProfileController.php

--- CasierConnecte/app/Http/Controllers/ProfileController.php
+++ CasierConnecte/app/Http/Controllers/ProfileController.php
@@ -1,5 +1,7 @@
 <?php
 
+// Auteur : Dylan Marty
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
@@ -12,7 +14,7 @@ use Illuminate\View\View;
 class ProfileController extends Controller
 {
     /**
-     * Display the user's profile form.
+     * Affiche le formulaire de profil de l'utilisateur.
      */
     public function edit(Request $request): View
     {
@@ -22,39 +24,49 @@ class ProfileController extends Controller
     }
 
     /**
-     * Update the user's profile information.
+     * Met à jour les informations de profil de l'utilisateur.
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
+        // Met à jour les informations du profil avec les données validées
         $request->user()->fill($request->validated());
 
+        // Réinitialise la vérification de l'e-mail si l'e-mail a été modifié
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
 
+        // Enregistre les modifications du profil
         $request->user()->save();
 
+        // Redirige vers la page de modification du profil avec un message de succès
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
 
     /**
-     * Delete the user's account.
+     * Supprime le compte de l'utilisateur.
      */
     public function destroy(Request $request): RedirectResponse
     {
+        // Valide le mot de passe de l'utilisateur pour la suppression du compte
         $request->validateWithBag('userDeletion', [
             'password' => ['required', 'current_password'],
         ]);
 
+        // Récupère l'utilisateur actuel
         $user = $request->user();
 
+        // Déconnecte l'utilisateur
         Auth::logout();
 
+        // Supprime le compte de l'utilisateur
         $user->delete();
 
+        // Invalide la session et régénère le jeton de session
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
+        // Redirige vers la page d'accueil après la suppression du compte
         return Redirect::to('/');
     }
 }
