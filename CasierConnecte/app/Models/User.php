42800cfc1c3934b4a9bfbb03de65e9eaf512a3e0
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:54:30 +0200

    Update User.php

--- CasierConnecte/app/Models/User.php
+++ CasierConnecte/app/Models/User.php
@@ -1,5 +1,7 @@
 <?php
 
+// Auteur : Dylan Marty
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +13,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Les attributs qui sont assignables en masse.
      *
      * @var array<int, string>
      */
@@ -23,7 +25,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * Les attributs qui sont cachés pour la sérialisation.
      *
      * @var array<int, string>
      */
@@ -33,7 +35,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * Obtient les attributs qui doivent être transformés.
      *
      * @return array<string, string>
      */
@@ -44,13 +46,24 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    /**
+     * Vérifie si l'utilisateur est un super-utilisateur.
+     *
+     * @return bool
+     */
     public function isSuperUtilisateur(): bool
     {
         return $this->droit === 'Super-Utilisateur';
     }
+
+    /**
+     * Vérifie si l'utilisateur est un super-utilisateur ou un responsable.
+     *
+     * @return bool
+     */
     public function isSuperUtilisateurOrResponsable(): bool
     {
         return $this->droit === 'Super-Utilisateur' || $this->droit === 'Responsable';
     }
-
 }
