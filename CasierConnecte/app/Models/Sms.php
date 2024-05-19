c5ad7c78332e55c4b2a6b1fc48d3810e9f032912
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:53:52 +0200

    Update Sms.php

--- CasierConnecte/app/Models/Sms.php
+++ CasierConnecte/app/Models/Sms.php
@@ -1,17 +1,29 @@
 <?php
 
+// Auteur : Dylan Marty
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 class Sms extends Model
 {
     use HasFactory;
-    protected $fillable = ['message', 'date_envoi', 'id_emprunt'];
+
+    /**
+     * Obtient l'emprunt associé à ce SMS.
+     */
     public function emprunt(): BelongsTo
     {
         return $this->belongsTo(Emprunt::class);
     }
 
+    /**
+     * Les attributs qui sont assignables en masse.
+     *
+     * @var array
+     */
+    protected $fillable = ['message', 'date_envoi', 'id_emprunt'];
 }
