3243eb0586eba969ebea6e2c8563e812e2fa1870
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-19 17:52:32 +0200

    Update Emprunt.php

--- CasierConnecte/app/Models/Emprunt.php
+++ CasierConnecte/app/Models/Emprunt.php
@@ -1,5 +1,7 @@
 <?php
 
+// Auteur : Dylan Marty
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,23 +9,33 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-
 class Emprunt extends Model
 {
     use HasFactory;
+
     protected $fillable = ['id', 'date_emprunt', 'date_retour', 'id_adherent', 'id_materiel'];
 
+    /**
+     * Obtient les SMS associés à cet emprunt.
+     */
     public function sms(): HasMany
     {
         return $this->hasMany(Sms::class);
     }
+
+    /**
+     * Obtient l'adhérent associé à cet emprunt.
+     */
     public function adherent(): BelongsTo
     {
         return $this->belongsTo(Adherent::class, 'id_adherent', 'id');
     }
+
+    /**
+     * Obtient le matériel associé à cet emprunt.
+     */
     public function materiel(): BelongsTo
     {
         return $this->belongsTo(Materiel::class, 'id_materiel', 'id');
     }
-
 }
