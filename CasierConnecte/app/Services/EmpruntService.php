b5acedacdee0114fd4efa01abc5f9afbfb4cacee
dylanmarty <112684087+dylanmarty@users.noreply.github.com>
2024-05-20 14:26:06 +0200

    Update EmpruntService.php

--- CasierConnecte/app/Services/EmpruntService.php
+++ CasierConnecte/app/Services/EmpruntService.php
@@ -38,8 +38,7 @@ class EmpruntService
                 $materielNom = $emprunt->materiel->nom;
 
                 // Crée le message SMS
-                $message = 'Bonjour, vous avez oublié de rendre votre matériel : "' . $materielNom . '".
-                Merci de le retourner dès que possible. Ceci est un message automatique. Merci de ne pas y répondre.';
+                $message = 'Bonjour, vous avez oublié de rendre votre matériel : "' . $materielNom . '".Merci de le retourner dès que possible. Ceci est un message automatique. Merci de ne pas y répondre.';
 
                 // Envoie le SMS
                 $this->smsService->sendSms($phoneNumber, $message);
